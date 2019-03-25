11e9c1aa62033f85899891c16059177e47849142
kwongjk <45679371+kwongjk@users.noreply.github.com>
2019-03-25 12:28:54 +0800

    Add files via upload

---
+++ Live Chat.js
@@ -0,0 +1,103 @@
+
+window.purechatApi = { l: [], t: [], on: function () { this.l.push(arguments); } };
+(function () {
+    var done = false; var script = document.createElement('script');
+    script.async = true; script.type = 'text/javascript';
+    script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript';
+    document.getElementsByTagName('HEAD').item(0).appendChild(script);
+    script.onreadystatechange = script.onload = function (e) {
+        if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({ c: 'cb7eac8d-410a-43e8-9669-ea2c15e27c05', f: true }); done = true; }
+    };
+})();
+
+
+
+
+
+function popupF() {
+    alert("😓Sorry!!! This Package is fully Booked.😓 \nIf have any question or issues, you can email us or live chat with us. \nThank you for using our website. 😄😄😄");
+}
+
+
+
+$(function () {
+    setInterval(function () {
+        $('#slider0').animate({ left: 0 }, 500, function () {
+            $('#slider0 img:first-child').appendTo('#slider0');
+            $('#slider0 img').css('left', '0');
+
+        });
+    }, 3000);
+});
+$(function () {
+    setInterval(function () {
+        $('#slider1').animate({ left: 0 }, 500, function () {
+            $('#slider1 img:first-child').appendTo('#slider1');
+            $('#slider1 img').css('left', '0');
+
+        });
+    }, 3000);
+});
+$(function () {
+    setInterval(function () {
+        $('#slider2').animate({ left: 0 }, 500, function () {
+            $('#slider2 img:first-child').appendTo('#slider2');
+            $('#slider2 img').css('left', '0');
+
+        });
+    }, 3000);
+});
+$(function () {
+    setInterval(function () {
+        $('#slider3').animate({ left: 0 }, 500, function () {
+            $('#slider3 img:first-child').appendTo('#slider3');
+            $('#slider3 img').css('left', '0');
+
+        });
+    }, 3000);
+});
+$(function () {
+    setInterval(function () {
+        $('#slider4').animate({ left: 0 }, 500, function () {
+            $('#slider4 img:first-child').appendTo('#slider4');
+            $('#slider4 img').css('left', '0');
+
+        });
+    }, 3000);
+});
+$(function () {
+    setInterval(function () {
+        $('#slider5').animate({ left: 0 }, 500, function () {
+            $('#slider5 img:first-child').appendTo('#slider5');
+            $('#slider5 img').css('left', '0');
+
+        });
+    }, 3000);
+});
+$(function () {
+    setInterval(function () {
+        $('#slider6').animate({ left: 0 }, 500, function () {
+            $('#slider6 img:first-child').appendTo('#slider6');
+            $('#slider6 img').css('left', '0');
+
+        });
+    }, 3000);
+});
+$(function () {
+    setInterval(function () {
+        $('#slider7').animate({ left: 0 }, 500, function () {
+            $('#slider7 img:first-child').appendTo('#slider7');
+            $('#slider7 img').css('left', '0');
+
+        });
+    }, 3000);
+});
+$(function () {
+    setInterval(function () {
+        $('#slider8').animate({ left: 0 }, 500, function () {
+            $('#slider8 img:first-child').appendTo('#slider8');
+            $('#slider8 img').css('left', '0');
+
+        });
+    }, 3000);
+});
